1c3d3f0953cb6a8cb0363cf51eb7c254af75e417
Jung Sumin <drt00784@gmail.com>
2025-05-04 22:12:23 +0900

    Chore: Chat 테이블의 Room 이름의 위치를 변경한다

---
+++ Migrations/20250504130455_ChangeChatRoomNameField.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Renty.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeChatRoomNameField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RoomName",
+                table: "ChatRooms");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RoomName",
+                table: "ChatUsers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RoomName",
+                table: "ChatUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RoomName",
+                table: "ChatRooms",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
